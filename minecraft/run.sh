20aae3656323433fd3467da18c300687a274be2f
Jimmy Gozal <jgozal@developerkit.co>
2016-12-03 22:06:37 +0000

    added NBT support

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx750M -Xms750M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar minecraft_server.1.11.jar nogui || netcat -l -k 25565
+java -Xmx700M -Xms700M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar minecraft_server.1.11.jar nogui || netcat -l -k 25565
